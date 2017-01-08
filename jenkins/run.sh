9f0c781bd53268f13fc9e325fc63fe3a861aeb30
PHOENIX-MKIII <junlapong@PHOENIX-MKIIIs-MacBook-Pro.local>
2017-01-08 23:41:50 +0700

    add elk-x-packt, jenkins, splunk

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,1 +1,8 @@
-docker run --name jenkins --rm -ti -p 8080:8080 -p 50000:50000 -v /Users/junlapong/docker/jenkins/home:/var/jenkins_home jenkins:2.32.1-alpine
+
+#!/usr/bin/env bash
+docker run --name jenkins \
+    --rm -ti \
+    -p 8080:8080 -p 50000:50000 \
+    jenkins:2.32.1-alpine
+
+#    -v $(pwd)/home:/var/jenkins_home \
