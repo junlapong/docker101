e3e46d78ef1f44480392702fdd90a0ec02f58b65
PHOENIX-MKIII <junlapong@PHOENIX-MKIIIs-MacBook-Pro.local>
2017-03-18 12:33:01 +0700

    update timezone

--- junlapong/woocommerce/run.sh
+++ junlapong/woocommerce/run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Start..."
+echo "Start... > http://localhost:8080/"
 
 docker run --name woocommerce \
   --rm -ti \
-  -v /etc/localtime:/etc/localtime \
-  -v $(pwd)/www/index.php:/var/www/index.php:ro \
   -p 8088:80 \
   junlapong/woocommerce
 
+
+#  -v /etc/localtime:/etc/localtime \
+#  -v $(pwd)/www/index.php:/var/www/index.php:ro \
 # docker exec -it woocommerce /bin/bash
